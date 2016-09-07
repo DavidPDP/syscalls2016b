f706f7a82773b646acb05c07a81e397691eb9404
DavidPDP <ballesto_@hotmail.com>
2016-09-06 23:49:35 -0500

    Create Syscall.c

---
+++ 13103036/Syscall.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <fcntl.h>
+
+main() {
+printf("Soy una máquina con oráculo\n");
+    int infectado = fork();
+        if(infectado == 0){
+            time_t fugaz = time(NULL);
+            printf("La velocidad del rasho: ");
+            printf(ctime(&fugaz));
+        }
+        else{
+            wait(0);
+            printf("Final...\n");
+            int parcialApoUno = creat("PreguntaParcialSegura", 0600);
+            write(parcialApoUno, "Pregunta 1. ¿p=np?", 18);
+        }
+}
